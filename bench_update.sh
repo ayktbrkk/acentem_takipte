d7ffede579d6896276149e1a7d506b8fadf349a3
ayktbrkk <ayktbrkk@users.noreply.github.com>
2026-04-27 15:57:44 +0300

    fix(deploy): added deep clean to bench_update.sh to force asset refresh

--- bench_update.sh
+++ bench_update.sh
@@ -2,6 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd ~/frappe-bench
+rm -rf apps/acentem_takipte/acentem_takipte/public/frontend/*
 rm -rf sites/assets/acentem_takipte
 bench build --app acentem_takipte
 bench --site at.localhost migrate
