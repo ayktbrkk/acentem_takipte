f008e52233f5ec0c9cd6e6cfa424398cee55c7c9
ayktbrkk <ayktbrkk@users.noreply.github.com>
2026-05-06 20:49:16 +0300

    docs: add missing nginx start command to frontend entrypoint example

--- docs/examples/docker-entrypoint.frontend.coolify.example.sh
+++ docs/examples/docker-entrypoint.frontend.coolify.example.sh
@@ -22,3 +22,5 @@ python3 /usr/local/bin/sync-assets-manifest.py
 envsubst '${BACKEND} ${SOCKETIO} ${FRAPPE_SITE_NAME_HEADER} ${UPSTREAM_REAL_IP_ADDRESS} ${UPSTREAM_REAL_IP_HEADER} ${UPSTREAM_REAL_IP_RECURSIVE} ${PROXY_READ_TIMEOUT} ${CLIENT_MAX_BODY_SIZE}' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
+
+exec nginx -g "daemon off;"
